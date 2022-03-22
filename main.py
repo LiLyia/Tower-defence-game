3fd554d01addb502759bd1b3b8a5a19eb6711abb
Gözde Kurtulmus <aa7vy7@inf.elte.hu>
2022-03-22 14:25:15 +0000

    merge

---
+++ main.py
@@ -0,0 +1,89 @@
+import pygame
+from pygame.locals import *
+from castle import Castle
+from game_map import GameMap
+from unit import Unit
+import random
+
+
+# -----------Colors------------------------#
+
+WHITE = (255, 255, 255)
+
+# ------------------game------------------#
+screen = pygame.init()
+SCREEN_HEIGHT, SCREEN_WIDTH = 650, 650
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Defence Tower")
+#  -----------Adjusting the Frame of the Game------------#
+clock = pygame.time.Clock()
+FPS = 60
+# -----------BG Image---------------------#
+bg_img = pygame.image.load("Images/bg.png")
+
+# -------------castle-class-----------------------
+# image of castle1 with 100% health
+castle1_100_img = pygame.image.load('Images/Castle/castle1_100.png').convert_alpha()
+# image of castle1 with 50% health
+castle1_50_img = pygame.image.load('Images/Castle/castle1_50.png').convert_alpha()
+# image of castle1 with 25% health
+castle1_25_img = pygame.image.load('Images/Castle/castle1_25.png')
+# image of castle2 with 100% health
+castle2_100_img = pygame.image.load('Images/Castle/castle2_100.png').convert_alpha()
+# image of castle2 with 50% health
+castle2_50_img = pygame.image.load('Images/Castle/castle2_50.png').convert_alpha()
+# image of castle2 with 25% health
+castle2_25_img = pygame.image.load('Images/Castle/castle2_25.png')
+# randomly picking the position of castle 1 position
+castle1_pos = [(150, 100), (200, 100), (250, 100), (300, 100), (350, 100), (400, 100), (450, 100)]
+position_castle1 = random.choice(castle1_pos)
+# randomly picking the position of castle 2 position
+castle2_pos = [(150, 450), (200, 450), (250, 450), (300, 450), (350, 450), (400, 450), (450, 450)]
+position_castle2 = random.choice(castle2_pos)
+castle1 = Castle(castle1_100_img,castle1_50_img,castle1_25_img, position_castle1, 0.09, screen)
+castle2 = Castle(castle2_100_img,castle2_50_img,castle2_25_img, position_castle2, 0.09, screen)
+
+soldier_img = pygame.image.load('Images/soldier.png')
+soldier = Unit(soldier_img, (100, 400), 0.07, screen)
+# ------------creating grid for game map --------------#
+tile_size = 50
+# def create_grid():
+#     for line in range(13):
+#         pygame.draw.line(screen, WHITE, (0, line*tile_size), (SCREEN_WIDTH, line* tile_size))
+#         pygame.draw.line(screen,WHITE, (line*tile_size, 0),(line * tile_size,SCREEN_HEIGHT))
+
+# -----------------Game Map---------------------
+game_map_data = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 5, 4, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1],
+    [1, 0, 2, 2, 0, 0, 0, 0, 0, 3, 2, 0, 1],
+    [1, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+game_map = GameMap(game_map_data, tile_size, screen)
+# make sure screen continue
+is_game = True
+while is_game:
+    clock.tick(FPS)
+    screen.blit(bg_img, (0, 0))
+    castle1.draw_castle()
+    castle2.draw_castle()
+    soldier.draw()
+    # create_grid()
+    game_map.draw_tiles()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            is_game = False
+        elif event.type == MOUSEBUTTONDOWN:
+            print(pygame.mouse.get_pos())
+
+    pygame.display.update()
+pygame.quit()
