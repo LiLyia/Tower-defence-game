bc120b9b437a660733e90de3833b3fa8e02b8e70
Ramzan Muhammad <pn8uom@inf.elte.hu>
2022-04-23 20:19:00 +0000

    Update the game_Mape

--- game_map.py
+++ game_map.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.locals import *
+# @ramzan
 
 
 class GameMap:
@@ -7,47 +7,29 @@ class GameMap:
         # Creating the list storing the position of the tiles
         self.tiles_position_list = []
         self.screen = screen
-        #         dirt and hurdles images
+        # Different images for game map
         dirt_img = pygame.image.load('Images/hurdles/dirt_0.png')
         hurdle1_img = pygame.image.load('Images/hurdles/hurdle_1.png')
         hurdle2_img = pygame.image.load('Images/hurdles/hurdle_2.png')
         hurdle3_img = pygame.image.load('Images/hurdles/hurdle_3.png')
         hurdle4_img = pygame.image.load('Images/hurdles/hurdle_4.png')
+        # creating dictionary for storing images
+        TITLE_IMAGE_MAP = {
+                        "1": dirt_img,
+                        "2": hurdle1_img,
+                        "3": hurdle2_img,
+                        "4": hurdle3_img,
+                        "5": hurdle4_img,
+                    }
+
         # for storing row count
         row_cnt = 0
         for row in data:
             col_cnt = 0
             for tile in row:
-                if tile == 1:
-                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_cnt * tile_size
-                    img_rect.y = row_cnt * tile_size
-                    tile = (img, img_rect)
-                    self.tiles_position_list.append(tile)
-                if tile == 2:
-                    img = pygame.transform.scale(hurdle1_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_cnt * tile_size
-                    img_rect.y = row_cnt * tile_size
-                    tile = (img, img_rect)
-                    self.tiles_position_list.append(tile)
-                if tile == 3:
-                    img = pygame.transform.scale(hurdle2_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_cnt * tile_size
-                    img_rect.y = row_cnt * tile_size
-                    tile = (img, img_rect)
-                    self.tiles_position_list.append(tile)
-                if tile == 4:
-                    img = pygame.transform.scale(hurdle3_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_cnt * tile_size
-                    img_rect.y = row_cnt * tile_size
-                    tile = (img, img_rect)
-                    self.tiles_position_list.append(tile)
-                if tile == 5:
-                    img = pygame.transform.scale(hurdle4_img, (tile_size, tile_size))
+                if tile in TITLE_IMAGE_MAP.keys():
+                    raw_img = TITLE_IMAGE_MAP[tile]
+                    img = pygame.transform.scale(raw_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
                     img_rect.x = col_cnt * tile_size
                     img_rect.y = row_cnt * tile_size
