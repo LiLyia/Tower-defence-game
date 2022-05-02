319b928c6149dd302277f9898025a08221c7e7db
Elbir Erberk <ao3i42@inf.elte.hu>
2022-05-02 14:35:41 +0000

    Update gameEngine_test.py

--- gameEngine_test.py
+++ gameEngine_test.py
@@ -10,7 +10,9 @@ from gui_button import Button
 ##################################################################
 SCREEN_HEIGHT, SCREEN_WIDTH = 650, 850
 screen = pygame.init()
+print(pygame.display.list_modes())
 screen = pygame.display.set_mode(size=(0,0),flags=0,depth=0,display=0,vsync=0)
+
 #screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 imager = imageCreator.ImageCreator.createImageCreator('Images')
 game_map_data = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],[1, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],[1, 0, 0, 0, 0, 0, 5, 4, 0, 0, 0, 0, 1],
